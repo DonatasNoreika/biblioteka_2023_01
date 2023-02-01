55fc254ca64b3b1657365650b31da362ece17391
Donatas Noreika <donoras@gmail.com>
2023-02-01 10:39:21 +0200

    12: padaryti vertimai views.py faile, registracijoje

--- mysite/library/views.py
+++ mysite/library/views.py
@@ -11,7 +11,7 @@ from django.views.generic.edit import FormMixin
 from .forms import BookReviewForm, UserUpdateForm, ProfilisUpdateForm, UserBookInstanceCreateForm
 from django.contrib.auth.decorators import login_required
 import datetime
-
+from django.utils.translation import gettext as _
 
 # Create your views here.
 
@@ -153,18 +153,22 @@ def register(request):
         password2 = request.POST['password2']
         if password == password2:
             if User.objects.filter(username=username).exists():
-                messages.error(request, f'Vartotojo vardas {username} užimtas!')
+                # messages.error(request, _(f'Vartotojo vardas {username} užimtas!'))
+                messages.error(request, _('Username %s already exists!') % username)
                 return redirect('register')
             else:
                 if User.objects.filter(email=email).exists():
-                    messages.error(request, f'Vartotojas su el. paštu {email} jau užregistruotas!')
+                    # messages.error(request, _(f'Vartotojas su el. paštu {email} jau užregistruotas!'))
+                    messages.error(request, _('Email %s already exists!') % email)
                     return redirect('register')
                 else:
                     User.objects.create_user(username=username, email=email, password=password)
-                    messages.info(request, f'Vartotojas {username} užregistruotas!')
+                    # messages.info(request, _(f'Vartotojas {username} užregistruotas!'))
+                    messages.info(request, _('User %s registered!') % username)
                     return redirect('login')
         else:
-            messages.error(request, 'Slaptažodžiai nesutampa!')
+            # messages.error(request, _('Slaptažodžiai nesutampa!'))
+            messages.error(request, _('Passwords do not match!'))
             return redirect('register')
     else:
         return render(request, 'registration/register.html')
